af24606a547eb0f52eb0ecceccf2a2f3249926e7
tony-blackie <jim.casey321@gmail.com>
2017-07-23 12:27:23 +0300

    Update all the stuff, add routes and root component

--- angular2/app/main.ts
+++ angular2/app/main.ts
@@ -1,14 +1,19 @@
 import { bootstrap }    from '@angular/platform-browser-dynamic';
 import { HTTP_PROVIDERS } from '@angular/http';
-import { RouterModule, Routes } from '@angular/router';
+import { provideRouter, RouterConfig } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { Root } from './components/root/root.component';
 
-const appRoutes: Routes = [
+const appRoutes: RouterConfig = [
     {
-        path: '/',
+        path: 'user',
         component: AppComponent
     }
 ];
 
-bootstrap(appRoutes, [HTTP_PROVIDERS]);
+const routeProviders = [
+    provideRouter(appRoutes)
+];
+
+bootstrap(Root, [HTTP_PROVIDERS, routeProviders]);
