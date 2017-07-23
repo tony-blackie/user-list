6e80e6952de90a9e9ea198a434e046bbad97611c
tony-blackie <jim.casey321@gmail.com>
2017-07-23 12:27:23 +0300

    Revert "Update all the stuff, add routes and root component"
    This reverts commit af24606a547eb0f52eb0ecceccf2a2f3249926e7.

--- angular2/app/main.ts
+++ angular2/app/main.ts
@@ -1,19 +1,14 @@
 import { bootstrap }    from '@angular/platform-browser-dynamic';
 import { HTTP_PROVIDERS } from '@angular/http';
-import { provideRouter, RouterConfig } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { Root } from './components/root/root.component';
 
-const appRoutes: RouterConfig = [
+const appRoutes: Routes = [
     {
-        path: 'user',
+        path: '/',
         component: AppComponent
     }
 ];
 
-const routeProviders = [
-    provideRouter(appRoutes)
-];
-
-bootstrap(Root, [HTTP_PROVIDERS, routeProviders]);
+bootstrap(appRoutes, [HTTP_PROVIDERS]);
